e9f0416047a5049874a6c9e4e1df58a95d283124
Aqsin <aqsin@local.dev>
2026-05-06 19:40:04 +0300

    Run database setup during WSGI startup

--- config/wsgi.py
+++ config/wsgi.py
@@ -14,3 +14,29 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+
+def _run_startup_tasks():
+    if os.getenv('DJANGO_SKIP_STARTUP_TASKS', '').lower() in ('1', 'true', 'yes', 'on'):
+        return
+
+    from django.contrib.auth import get_user_model
+    from django.core.management import call_command
+
+    call_command('migrate', interactive=False, verbosity=0)
+
+    username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+    email = os.getenv('DJANGO_SUPERUSER_EMAIL', '')
+    password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+    if username and password:
+        User = get_user_model()
+        user, _ = User.objects.get_or_create(username=username, defaults={'email': email})
+        user.email = email
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
+
+
+_run_startup_tasks()
